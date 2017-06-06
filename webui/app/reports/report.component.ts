4b8da5862fde9f70b8794b3e8b7923417bda3d8e
Arun Sharma <sharma.arun333@gmail.com>
2017-06-06 14:41:27 -0400

    removed ngcharts from reports page

--- webui/app/reports/report.component.ts
+++ webui/app/reports/report.component.ts
@@ -36,7 +36,6 @@ export class ReportPage {
         });
     }
     ngOnDestroy() {
-        clearInterval(this.intervalCounter);
     }
 
     public projectName = "";
@@ -44,70 +43,13 @@ export class ReportPage {
     public loadedCharts = false;
     headers = new Headers({ 'Content-Type': 'application/json' });
 
-    public lineChartData: Array<any> = [
-        { data: [0], label: '' }
-    ];
-    public lineChartLabels: Array<any> = [''];
-    public lineChartOptions: any = {
-        animation: false,
-        responsive: true
-    };
-    public lineChartColors: Array<any> = [
-        { // grey
-            backgroundColor: 'rgba(148,159,177,0.2)',
-            borderColor: 'rgba(148,159,177,1)',
-            pointBackgroundColor: 'rgba(148,159,177,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-        },
-        { // blue
-            backgroundColor: 'rgba(0,0,255,0.2)',
-            borderColor: 'rgba(0,0,255,1)',
-            pointBackgroundColor: 'rgba(77,83,96,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(77,83,96,1)'
-        },
-        { // red
-            backgroundColor: 'rgba(255,0,0,0.2)',
-            borderColor: 'rgba(255,0,0,1)',
-            pointBackgroundColor: 'rgba(148,159,177,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-        }
-    ];
-
-    public lineChartType = "line";
-    public oldCount = [0];
-    public lcc = [0];
-    public runOnce = [true];
-    public reportChartMap = {};
-    intervalCounter = -1;
     public constructor(public http: Http, public route: ActivatedRoute) {
         var _this_ = this;
         if (this.projectName == undefined) {
             var uri = window.location.href.split["/"];
             this.projectName = uri[uri.length - 1];
         }
-        //load the plugins
-        setTimeout(function() {
-            _this_.charts.forEach(function(cc) {
-                _this_.chartsRef[cc.element.nativeElement.id] = cc;
-            });
-            _this_.loadedCharts = true;
-            console.log(_this_.chartsRef);
-
-        }, 500);
-        this.intervalCounter = setInterval(function() {
-            if (_this_.loadedCharts)
-                _this_.updateFunction();
-
-        }, 1000);
-
     }
-
     private getUpdates() {
         //fetch projects 
         var _this_ = this;
@@ -117,31 +59,7 @@ export class ReportPage {
                 if (dd != undefined && dd.length > 0) {
                     //add one chartline per report
 
-                    if (_this_.lineChartData.length != dd.length) {
-                        //mismatch found add more lines
-
-                        for (var ii = 0; ii < dd.length; ii++) {
-                            if (ii > 0) _this_.lineChartData.push({ data: [0], label: '' });
-                            var d = JSON.parse(dd[ii]);
-                            _this_.reportChartMap[d.report] = ii;
-                            _this_.runOnce[ii] = true;
-                            _this_.oldCount[ii] = 0;
-                        }
-
-                    } else if(_this_.runOnce[0]){
-                        var d = JSON.parse(dd[0]);
-                        _this_.reportChartMap[d.report] = 0;
-                        _this_.runOnce[0] = true;
-                        _this_.oldCount[0] = 0;
-                    }
-
-                    for (var i = 0; i < dd.length; i++) {
-                        console.log(dd[i]);
-                        var d = JSON.parse(dd[i]);
-                        console.log(d);
-                        _this_.updateChartsData(_this_, d);
-
-                    }
+                    
                 }
             }, e => {
                 console.log("Cound not fetch projects list!");
@@ -151,41 +69,7 @@ export class ReportPage {
         );
     }
 
-    public updateChartsData(_this_, d) {
-        var _this_ = _this_;
-        var nc = 0;
-        var i = _this_.reportChartMap[d.report];
-        console.log("updating for : " + d.report + " [" + i + "]");
-        console.log(_this_.runOnce[i] + " and " + _this_.oldCount[i]);
-        if (_this_.runOnce[i]) {
-            console.log("running Once");
-            _this_.oldCount[i] = d.data.count;
-            _this_.runOnce[i] = false;
-        } else {
-            console.log("again ...");
-            if (_this_.oldCount[i] != d.data.count) {
-
-                nc = d.data.count - _this_.oldCount[i];
-            } else {
-                nc = 0;
-
-            }
-        }
-        console.log(nc);
-        _this_.appendVal(_this_.lineChartData[i].data, nc);
-        if (i == 0) _this_.appendVal(_this_.lineChartLabels, d.data.date);
-        _this_.lineChartData[i].label = d.data.stream_label;
-        _this_.oldCount[i] = d.data.count;
-    }
-
-    public updateFunction() {
-        var _this_ = this;
-        _this_.getUpdates();
-        //_this_.appendRandom(_this_.lineChartData[0].data);
-        _this_.chartsRef.lineChart.ngOnChanges({});
-
-    }
-
+    
     private appendRandom(arr) {
         var tmp = 0;
         for (var i = 0; i < arr.length; i++) {
@@ -196,20 +80,5 @@ export class ReportPage {
         }
         arr[arr.length - 1] = Math.random() * 100;
     }
-
-    private appendVal(arr, val) {
-        //        var tmp = 0;
-        //        console.log(arr.length);
-        //        for (var i = 0; i < arr.length; i++) {
-        //            tmp = arr[i];
-        //            if (i < arr.length - 1) {
-        //                arr[i] = arr[i + 1];
-        //            }
-        //        }
-        //        arr[arr.length - 1] = val;
-        arr.push(val);
-    }
-
-
 
 }
